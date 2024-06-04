e1ac21fad416f2d91e11fa98a08b350774247bf8
Towkir <towkirmiah5@gmail.com>
2024-06-04 17:06:46 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import Search from './Search'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='w-100 main-wrapper d-flex flex-column align-items-center
+    justify-content-center'> 
+    <header>
+      <h2>Watch Movies</h2>
+      <Search />
+    </header>
+    
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Search.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <div className='search-input'>
+      <input type='text' name='movie' className='w-50 p-2'
+      placeholder='Search Movie ...'></input>
+    </div>
+  )
+}
+
+export default Search
